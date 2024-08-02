06df9a0f172d0d9955caf8a6422611ce3cd99149
Richard T. Jones <jonesrt@gluey.phys.uconn.edu>
2024-08-02 03:09:33 -0400

    * eliminate the building of python2 modules for the models [rtj]

--- scripts/python_module_builder.sh
+++ scripts/python_module_builder.sh
@@ -4,11 +4,16 @@ while [ $# -gt 1 ]; do
     echo $1
     shift
 done
-if [ "$(which python2)" != "" ]; then
-    python2 $1
-else
-    python $1
-fi
+
+# This old python2 module builder is disabled
+#if [ "$(which python2)" != "" ]; then
+#    python2 $1
+#else
+#    python $1
+#fi
+
 if [ "$(which python3)" != "" ]; then
     python3 $1
+else
+    python $1
 fi
